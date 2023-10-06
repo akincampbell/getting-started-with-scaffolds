180bd26f0aedadd8ac9de89d23dc4dc35cbc4d29
Akin Campbell <54187807+akincampbell@users.noreply.github.com>
2023-10-06 23:28:53 +0000

    Add books use "id" instead of "path_id"

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,7 @@ class MoviesController < ApplicationController
   end
 
   def show
-    the_id = params.fetch("path_id")
+    the_id = params.fetch("id")
 
     matching_movies = Movie.where({ :id => the_id })
 
@@ -33,8 +33,8 @@ class MoviesController < ApplicationController
   end
 
   def update
-    the_id = params.fetch("path_id")
-    the_movie = Movie.where({ :id => the_id }).at(0)
+    the_id = params.fetch("id")
+    the_movie = Movie.where({ :id => the_id })[0]
 
     the_movie.title = params.fetch("query_title")
     the_movie.description = params.fetch("query_description")
@@ -49,8 +49,8 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    the_id = params.fetch("path_id")
-    the_movie = Movie.where({ :id => the_id }).first
+    the_id = params.fetch("id")
+    the_movie = Movie.where({ :id => the_id })[0]
 
     the_movie.destroy
 
